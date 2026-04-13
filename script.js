4c6948130ffd89534cdd111ce4f0097570332e72
I1HSA <menuzzobiel@icloud.com>
2026-04-13 11:14:40 -0300

    Segundo exercício

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-let num;
-num = Number(prompt("Digite um número qualquer: "));
+let valor;
+valor = Number(prompt("Insira o valor da sua compra: "));
 
-if (isNaN(num)) {
+if (isNaN(valor)) {
 
     alert("Você não digitou um número!")
 
 }
 else {
 
-    if (num % 2 == 0) {
+    if (valor >= 150) {
 
-        alert(num + " é um número par.")
+        let desc;
+        desc = Number(valor * 0.02);
+        let vCdesc;
+        vCdesc = Number(valor * 0.98);
+
+        alert("Parabéns! Você recebeu um desconto de " + desc.toFixed(2) + " reais");
+        alert("O valor do produto com o desconto é igual a " + vCdesc.toFixed(2) + " reais");
 
     }
     else {
 
-        alert(num + " é um número ímpar.")
+        let cashback;
+        cashback = Number(valor * 0.02);
+
+        if (valor == 1) {
+
+            alert("O valor do produto é igual a " + valor + " real, com isso você recebeu um cashbak de " + cashback.toFixed(2) + " reais");
+
+        }
+
+        else {
+
+            if (cashback == 1) {
+
+                alert("O valor do produto é igual a " + valor + " reais, com isso você recebeu um cashbak de " + cashback.toFixed(2) + " real");
+
+            }
+            else {
+
+                alert("O valor do produto é igual a " + valor + " reais, com isso você recebeu um cashbak de " + cashback.toFixed(2) + " reais");
+
+            }
+
+        }
+
+
 
     }
-};
+}
