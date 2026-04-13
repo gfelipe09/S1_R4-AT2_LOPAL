57cc9a98f65b81c4c57d3eb2c8bad1432c1cdc43
I1HSA <menuzzobiel@icloud.com>
2026-04-13 12:45:20 -0300

    Indentação do código

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ altura = Number(prompt("Insira a sua altura em metros: "));
 
 if (altura >= 1.40) {
 
-alert("Você pode entrar no brinquedo.")
+    alert("Você pode entrar no brinquedo.")
 
 }
 else {
 
-alert("Altura insuficiente.")
+    alert("Altura insuficiente.")
 
 };
